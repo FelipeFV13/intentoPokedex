4969468ebc16c71d95f0db699ff59bc7d6ec790a
sebastian <sabc1087@gmail.com>
2024-10-31 19:51:04 -0500

    primeros pasos

---
+++ src/main/java/com/mycompany/pokeperro/Pokeperro.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pokeperro;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Pokeperro {
+    public Clip clip;
+    public String ruta = "./Charmander es un Pok.mp3";
+
+    public static void main(String[] args) {
+        
+    }
+}
